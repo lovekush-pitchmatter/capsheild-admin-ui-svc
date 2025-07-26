9680726e096ffc4731757a8f0a415bc787401942
Vaishnaaaa <vishnuanilkumar@gmail.com>
2025-07-26 20:38:48 +0530

    login

--- src/components/LoginLeft.tsx
+++ src/components/LoginLeft.tsx
@@ -1,14 +1,13 @@
+import React from "react";
 import Logo from "../assets/logo.png";
-import bgImage from "../assets/login_bg.png";
 
 const LoginLeft = () => {
   return (
-    <div className="max-lg:hidden  w-1/2 relative overflow-hidden">
-      <div className="absolute  z-0 bg-gray-300 ">
-        <img src={bgImage} alt="bgimg" className="  opacity-45" />
+    <div className="max-lg:hidden w-1/2 bg-[#f5f6fa] flex items-center justify-center relative">
+      <div className="flex items-center justify-center h-full w-full">
+        <div className="rounded-full bg-gradient-to-br from-[#1a1a2e] to-[#6c47ff] shadow-xl flex items-center justify-center w-72 h-72">
+          <img src={Logo} alt="Logo" className="w-40 h-40 object-contain" />
       </div>
-      <div className="relative z-10 flex items-center justify-center h-full">
-        <img src={Logo} alt="Logo" className="w-20 h-20 lg:w-60 lg:h-60" />
       </div>
     </div>
   );
