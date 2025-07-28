28dd3042321b8d20fd7a0c136683a1b022cab158
Vaishnaaaa <vishnuanilkumar@gmail.com>
2025-07-28 10:58:50 +0530

    forgot password,logo changes

--- src/components/LoginLeft.tsx
+++ src/components/LoginLeft.tsx
@@ -3,11 +3,9 @@ import Logo from "../assets/logo.png";
 
 const LoginLeft = () => {
   return (
-    <div className="max-lg:hidden w-1/2 bg-[#f5f6fa] flex items-center justify-center relative">
-      <div className="flex items-center justify-center h-full w-full">
-        <div className="rounded-full bg-gradient-to-br from-[#1a1a2e] to-[#6c47ff] shadow-xl flex items-center justify-center w-72 h-72">
-          <img src={Logo} alt="Logo" className="w-40 h-40 object-contain" />
-      </div>
+    <div className="max-lg:hidden w-1/2 bg-[#f5f6fa] flex items-center justify-center">
+      <div className="flex items-center justify-center w-56 h-56 rounded-full bg-[#f5f6fa]">
+        <img src={Logo} alt="Logo" className="w-40 h-40 object-contain" />
       </div>
     </div>
   );
